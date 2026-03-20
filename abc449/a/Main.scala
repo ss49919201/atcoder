811d0d2eb7df63163ba721aedd8a2e7d0573a784
Shinya Sakae <ss49919201@gmail.com>
2026-03-20 16:47:11 +0900

    refactor

--- abc449/a/Main.scala
+++ abc449/a/Main.scala
@@ -2,19 +2,16 @@
 def run(): Unit = {
   val sc = new java.util.Scanner(System.in)
   val h, w, q = sc.nextInt()
-  val ql = List.fill(q, 2)(sc.nextInt())
+  val ql = List.fill(q)(sc.nextInt(), sc.nextInt())
 
-  val hState = ql.init.foldLeft(List(h))((m, n) => {
-    m.appended(m.last - (if n(0) == 2 then 0 else n(1)))
-  })
-  val wState = ql.init.foldLeft(List(w))((m, n) => {
-    m.appended(m.last - (if n(0) == 1 then 0 else n(1)))
-  })
-  hState
-    .zip(wState)
+  ql.scanLeft((h, w)) {
+    case ((h, w), (1, r)) => (h - r, w)
+    case ((h, w), (_, c)) => (h, w - c)
+  }.init
     .zip(ql)
-    .map((a) => {
-      (if a(1)(0) == 1 then a(0)(1) else a(0)(0)) * a(1)(1)
-    })
+    .map {
+      case ((_, w), (1, r)) => w * r
+      case ((h, _), (_, c)) => h * c
+    }
     .foreach(println)
 }
