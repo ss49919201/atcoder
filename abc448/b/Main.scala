205e894d95b5227b856ea9d959879926201ebfb1
Shinya Sakae <ss49919201@gmail.com>
2026-03-21 16:35:16 +0900

    move abc449 b

---
+++ abc448/b/Main.scala
@@ -0,0 +1,15 @@
+@main
+def f(): Unit = {
+  val sc = new java.util.Scanner(System.in)
+  val n, x = sc.nextInt()
+  val a = Array.fill(n)(sc.nextInt())
+  
+  val (_, result) = a.foldLeft((x, List[Int]()))((tuple, v) => {
+    if v < tuple._1 then
+      (v, tuple._2.appended(1))
+    else
+      (tuple._1, tuple._2.appended(0))
+  })
+
+  result.foreach(println)
+}
